553fc6209737768407aef9bf9bbe6afb68ee37cb
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-05-04 14:05:35 -0500

    extract overloads for oracle

--- awswrangler/oracle.py
+++ awswrangler/oracle.py
@@ -14,7 +14,6 @@ from typing import (
     Tuple,
     TypeVar,
     Union,
-    overload,
 )
 
 import boto3
@@ -180,50 +179,6 @@ def connect(
     return oracle_connection
 
 
-@overload
-def read_sql_query(
-    sql: str,
-    con: "oracledb.Connection",
-    index_col: Optional[Union[str, List[str]]] = ...,
-    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
-    chunksize: None = ...,
-    dtype: Optional[Dict[str, pa.DataType]] = ...,
-    safe: bool = ...,
-    timestamp_as_object: bool = ...,
-) -> pd.DataFrame:
-    ...
-
-
-@overload
-def read_sql_query(
-    sql: str,
-    con: "oracledb.Connection",
-    *,
-    index_col: Optional[Union[str, List[str]]] = ...,
-    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
-    chunksize: int,
-    dtype: Optional[Dict[str, pa.DataType]] = ...,
-    safe: bool = ...,
-    timestamp_as_object: bool = ...,
-) -> Iterator[pd.DataFrame]:
-    ...
-
-
-@overload
-def read_sql_query(
-    sql: str,
-    con: "oracledb.Connection",
-    *,
-    index_col: Optional[Union[str, List[str]]] = ...,
-    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
-    chunksize: Optional[int],
-    dtype: Optional[Dict[str, pa.DataType]] = ...,
-    safe: bool = ...,
-    timestamp_as_object: bool = ...,
-) -> Union[pd.DataFrame, Iterator[pd.DataFrame]]:
-    ...
-
-
 @_utils.check_optional_dependency(oracledb, "oracledb")
 def read_sql_query(
     sql: str,
@@ -288,53 +243,6 @@ def read_sql_query(
         safe=safe,
         timestamp_as_object=timestamp_as_object,
     )
-
-
-@overload
-def read_sql_table(
-    table: str,
-    con: "oracledb.Connection",
-    schema: Optional[str] = ...,
-    index_col: Optional[Union[str, List[str]]] = ...,
-    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
-    chunksize: None = ...,
-    dtype: Optional[Dict[str, pa.DataType]] = ...,
-    safe: bool = ...,
-    timestamp_as_object: bool = ...,
-) -> pd.DataFrame:
-    ...
-
-
-@overload
-def read_sql_table(
-    table: str,
-    con: "oracledb.Connection",
-    *,
-    schema: Optional[str] = ...,
-    index_col: Optional[Union[str, List[str]]] = ...,
-    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
-    chunksize: int,
-    dtype: Optional[Dict[str, pa.DataType]] = ...,
-    safe: bool = ...,
-    timestamp_as_object: bool = ...,
-) -> Iterator[pd.DataFrame]:
-    ...
-
-
-@overload
-def read_sql_table(
-    table: str,
-    con: "oracledb.Connection",
-    *,
-    schema: Optional[str] = ...,
-    index_col: Optional[Union[str, List[str]]] = ...,
-    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
-    chunksize: Optional[int],
-    dtype: Optional[Dict[str, pa.DataType]] = ...,
-    safe: bool = ...,
-    timestamp_as_object: bool = ...,
-) -> Union[pd.DataFrame, Iterator[pd.DataFrame]]:
-    ...
 
 
 @_utils.check_optional_dependency(oracledb, "oracledb")
---
+++ awswrangler/oracle.pyi
@@ -0,0 +1,110 @@
+# noqa: D100
+from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Literal, Optional, Tuple, Union, overload
+
+import boto3
+import pandas as pd
+import pyarrow as pa
+
+if TYPE_CHECKING:
+    try:
+        import oracledb
+    except ImportError:
+        pass
+
+def connect(  # noqa: D103
+    connection: Optional[str] = ...,
+    secret_id: Optional[str] = ...,
+    catalog_id: Optional[str] = ...,
+    dbname: Optional[str] = ...,
+    boto3_session: Optional[boto3.Session] = ...,
+    call_timeout: Optional[int] = ...,
+) -> "oracledb.Connection": ...
+@overload
+def read_sql_query(
+    sql: str,
+    con: "oracledb.Connection",
+    index_col: Optional[Union[str, List[str]]] = ...,
+    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
+    chunksize: None = ...,
+    dtype: Optional[Dict[str, pa.DataType]] = ...,
+    safe: bool = ...,
+    timestamp_as_object: bool = ...,
+) -> pd.DataFrame: ...
+@overload
+def read_sql_query(
+    sql: str,
+    con: "oracledb.Connection",
+    *,
+    index_col: Optional[Union[str, List[str]]] = ...,
+    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
+    chunksize: int,
+    dtype: Optional[Dict[str, pa.DataType]] = ...,
+    safe: bool = ...,
+    timestamp_as_object: bool = ...,
+) -> Iterator[pd.DataFrame]: ...
+@overload
+def read_sql_query(
+    sql: str,
+    con: "oracledb.Connection",
+    *,
+    index_col: Optional[Union[str, List[str]]] = ...,
+    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
+    chunksize: Optional[int],
+    dtype: Optional[Dict[str, pa.DataType]] = ...,
+    safe: bool = ...,
+    timestamp_as_object: bool = ...,
+) -> Union[pd.DataFrame, Iterator[pd.DataFrame]]: ...
+@overload
+def read_sql_table(
+    table: str,
+    con: "oracledb.Connection",
+    schema: Optional[str] = ...,
+    index_col: Optional[Union[str, List[str]]] = ...,
+    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
+    chunksize: None = ...,
+    dtype: Optional[Dict[str, pa.DataType]] = ...,
+    safe: bool = ...,
+    timestamp_as_object: bool = ...,
+) -> pd.DataFrame: ...
+@overload
+def read_sql_table(
+    table: str,
+    con: "oracledb.Connection",
+    *,
+    schema: Optional[str] = ...,
+    index_col: Optional[Union[str, List[str]]] = ...,
+    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
+    chunksize: int,
+    dtype: Optional[Dict[str, pa.DataType]] = ...,
+    safe: bool = ...,
+    timestamp_as_object: bool = ...,
+) -> Iterator[pd.DataFrame]: ...
+@overload
+def read_sql_table(
+    table: str,
+    con: "oracledb.Connection",
+    *,
+    schema: Optional[str] = ...,
+    index_col: Optional[Union[str, List[str]]] = ...,
+    params: Optional[Union[List[Any], Tuple[Any, ...], Dict[Any, Any]]] = ...,
+    chunksize: Optional[int],
+    dtype: Optional[Dict[str, pa.DataType]] = ...,
+    safe: bool = ...,
+    timestamp_as_object: bool = ...,
+) -> Union[pd.DataFrame, Iterator[pd.DataFrame]]: ...
+def to_sql(  # noqa: D103
+    df: pd.DataFrame,
+    con: "oracledb.Connection",
+    table: str,
+    schema: str,
+    mode: Literal["append", "overwrite"] = ...,
+    index: bool = ...,
+    dtype: Optional[Dict[str, str]] = ...,
+    varchar_lengths: Optional[Dict[str, int]] = ...,
+    use_column_names: bool = ...,
+    chunksize: int = ...,
+) -> None: ...
+def detect_oracle_decimal_datatype(cursor: Any) -> Dict[str, pa.DataType]: ...  # noqa: D103
+def handle_oracle_objects(  # noqa: D103
+    col_values: List[Any], col_name: str, dtype: Optional[Dict[str, pa.DataType]] = None
+) -> List[Any]: ...
